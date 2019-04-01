357d583fddc0bf949d0c1fa8c470e30541f1fc52
Gabriel Stamato <stamato7@gmail.com>
2019-04-01 16:40:23 -0300

    form dinamico (com erros)

--- src/pages/Order/Create/index.js
+++ src/pages/Order/Create/index.js
@@ -6,101 +6,115 @@ import PropTypes from "prop-types";
 import Header from "../../../components/header";
 
 class CreateOrder extends Component {
-    static propTypes = {
-        history: PropTypes.shape({
-            push: PropTypes.func
-        }).isRequired
-    };
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func
+    }).isRequired
+  };
 
-    state = {
-        desk: "",
-        ticket_id: '',
-        numComanda: '',
-        products: [],
-        product_orders: [{
-            order_id: '',
-            product_id: '',
-            qnt: ''
-        }],
-        addActivate: false
-    };
+  state = {
+    desk: "",
+    ticket_id: "",
+    numComanda: "",
+    products: [],
+    order_products: [
+      {
+        order_id: "",
+        product_id: "",
+        qnt: ""
+      }
+    ],
+    order: ''
+  };
 
-    async componentDidMount() {
-        const { id } = this.props.match.params;
+  async componentDidMount() {
+    const { id } = this.props.match.params;
 
-        const ticket = await api.get(`ticket/${id}`)
+    const ticket = await api.get(`ticket/${id}`);
+    const products = await api.get('products');
 
-        this.setState({ ticket_id: ticket.data.id, numComanda: ticket.data.numComanda })
+    this.setState({
+      ticket_id: ticket.data.id,
+      numComanda: ticket.data.numComanda,
+      inUse: ticket.data.inUse,
+      products: products.data
+    });
 
+    console.log(products)
+  }
+
+  handleAddProduct = (e) => {
+    this.setState({
+      order_products: this.state.order_products.concat([{ order_id: this.state.order.id, product_id: e.target.value, qnt: e.target.value }])
+    });
+  };
+
+  handleCreate = async e => {
+    e.preventDefault();
+
+    const { desk, ticket_id } = this.state;
+
+    if (!desk) {
+      this.setState({
+        error: "Preencha com o número da mesa para continuar"
+      });
     }
-
-    handleCreate = async e => {
-        e.preventDefault();
-        const { desk, ticket_id } = this.state;
-        if (!desk) {
-            this.setState({
-                error: "Preencha com o número da mesa para continuar"
-            });
-        } else {
-            try {
-                await api.post("/order", { desk, ticket_id });
-                this.setState({
-                    sucess: "Pedido cadastrado com sucesso!",
-                });
-                alert(this.state.sucess);
-            } catch {
-                this.setState({
-                    error: "Algo deu errado. Por favor, verifique os dados e tente novamente."
-                });
-            }
-        }
-    };
-
-    async handleAddProduct() {
-        const response = await api.get('/products');
-        this.setState({ products: response.data });
-        console.log(response)
-
-        return (
-                <form>
-                    <select>
-                        {this.state.products.map(product => (
-                            <option key={product.id}
-                                value={product.id}>
-                                {product.description}
-                            </option>
-                        ))}
-                    </select>
-                    <input
-                        type="number"
-                        placeholder="Quantidade"
-                        onChange={e => this.setState({ qnt: e.target.value })}
-                    />
-                </form>
-            )
+    if (this.state.inUse === true) {.value
+      alert(
+        "Comanda já está sendo utilizada. Por favor, selecione uma nova comanda!"
+      );
+      this.props.history.push("/tickets");
+    } else {
+      try {
+        const data = await api.post("/order", { desk, ticket_id });
+        this.setState({ order: data.data});
+      } catch {
+        this.setState({
+          error:
+            "Algo deu errado. Por favor, verifique os dados e tente novamente."
+        });
+      }
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <Container>
-                    <Form onSubmit={this.handleCreate}>
-                        <h1>Pedidos</h1>
-                        <hr />
-                        {this.state.error && <p>{this.state.error}</p>}
-                        <strong>Comanda nº: {this.state.numComanda}</strong>
-                        <input
-                            type="number"
-                            placeholder="Número da mesa"
-                            onChange={e => this.setState({ desk: e.target.value })}
-                        />
-                        <button type="submit" onClick={this.setState({ addActivate: true })}>Fazer Pedido</button>
-                    </Form>
-                </Container>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container>
+          <Form onSubmit={this.handleCreate}>
+            <h1>Pedidos</h1>
+            <hr />
+            {this.state.error && <p>{this.state.error}</p>}
+            <strong>Comanda nº: {this.state.numComanda}</strong>
+            <input
+              type="number"
+              placeholder="Número da mesa"
+              onChange={e => this.setState({ desk: e.target.value })}
+            />
+            <button onChange={this.handleAddProduct}>Fazer Pedido</button>
+            
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default withRouter(CreateOrder);
+
+
+/*<div>
+        <select>
+          {this.state.products.map(product => (
+            <option key={product.id} value={product.id}>
+              {product.description}
+            </option>
+          ))}
+        </select>
+        <input
+          type="number"
+          placeholder="Número da mesa"
+          onChange={e => this.setState({ desk: e.target.value })}
+        />
+      </div>*/
