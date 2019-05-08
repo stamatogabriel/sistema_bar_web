fad00528d74add610db739138ea83fd1fd1da300
Gabriel Henrique Stamato <stamato7@gmail.com>
2019-05-07 22:18:34 -0300

    Add Dropdown no Header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
+
+import { FaUserAlt } from 'react-icons/fa';
+
+import api from '../../services/api';
+
 import { Head } from "./styles";
 
 class Header extends Component {
-    state = {
-        user: ''
-    };
+  state = {
+    user: '',
+    showMenu: false
+  };
 
-    componentDidMount(){
+  async componentDidMount() {
+    const user = await api.get('/get_user');
+    this.setState({ user: user.data });
 
-    }
+    console.log(this.state.user)
+  }
 
+  showMenu = (e) => {
+    e.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu = () => {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu);
+    });
+  }
 
   render() {
     return (
@@ -17,10 +39,31 @@ class Header extends Component {
         <div>
           <h3>Sistema_Bar</h3>
         </div>
-        <div>
-            <div></div>
-            <span></span>
-        </div>
+        <div className='buttonContainer'>
+        <button onClick={this.showMenu}>
+        <FaUserAlt size={15} /> 
+           <span>{this.state.user.username}</span>
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <a> Menu item 1 </a>
+                <a> Menu item 2 </a>
+                <a> Menu item 3 </a>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
       </Head>
     );
   }
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -23,4 +23,58 @@ export const Head = styled.header`
     h3 {
         color: #7c7c7c;
     }
+
+    span {
+        font-size: 20px;
+    }
+
+    .buttonContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: right;
+        margin-right: 15px;
+        width: 30%;
+        padding: 15px;
+    }
+
+    .menu {
+        display: flex;
+        float: right;
+        flex-direction: column;
+        margin: 5px  auto;
+        color: #fff;
+        background-color: #190d18;
+        border-radius: 5px;
+        border: solid 0.5px #190d18;
+        font-size: 14px;
+        padding: 5px;
+        align-items: center;
+        a{
+            padding: 10px;
+        }
+    }
+
+    button {
+        display: flex;
+        flex-direction: row;
+        color: #7c7c7c;
+        background-color: #201e29;
+        justify-content: center;
+        border: none;
+        margin: auto;
+        width: 100%;
+        height: 40px;
+        border-radius: 5px;
+        font-size: 14px;
+        font-weight: bold;
+        cursor: pointer;
+        &:hover{
+            color: #fff;
+            background-color: #190d18;
+        }
+        span {
+            margin-left: 5px;
+            font-size: 16px;
+        }
+    }
     `;
